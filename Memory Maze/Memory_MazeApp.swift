4c3762275642771b61c2610864dcb13b2d64413d
Richy <garrincha33@users.noreply.github.com>
2023-03-03 06:34:16 +0000

    Initial Commit

---
+++ Memory Maze/Memory_MazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_MazeApp.swift
+//  Memory Maze
+//
+//  Created by Richard Price on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Memory_MazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
